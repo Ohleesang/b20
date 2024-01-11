f8d56f0808fe280ae85798e9a918003dfbad543c
Ohleesang <jammmmed@gmail.com>
2024-01-11 09:30:06 +0900

    간단한 배열 문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,14 @@
 class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer = IntArray(numbers.size)
+
+        for(idx in numbers.indices){
+            answer[idx] = numbers[idx] * 2
+        }
+        return answer
+    }
+}
+fun main(){
+    Solution().solution(intArrayOf(1,2,3,4,5))
+    Solution().solution(intArrayOf(1,2,100,-99,1,2,3))
 }
